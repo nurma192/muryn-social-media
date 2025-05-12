3db506c7ab79051c766cf5d83f17477cf1346559
Nurmukhamet Kenges <nurma192k@gmail.com>
2025-05-12 10:08:08 +0500

    fix image port

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,5 +7,5 @@ export function cn(...inputs: ClassValue[]) {
 
 export function change_minio(value: string | null | undefined) {
   if (!value) return "";
-  return value.replace(/^http:\/\/minio/, `http://34.40.19.17:9000`);
+  return value.replace(/^http:\/\/minio/, `http://34.40.19.17`);
 }
