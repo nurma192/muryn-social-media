cb79c26743ad00e67772133cda751ff6d1f10799
Nurmukhamet Kenges <nurma192k@gmail.com>
2025-05-12 09:28:21 +0500

    to https

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const IP = import.meta.env.VITE_API_IP;
-export const URL = `http://${IP}`;
+export const URL = `https://${IP}`;
 export const BASE_URL = `${URL}:8000`;
 export const IMAGE_BASE_URL = `${URL}:9000`;
