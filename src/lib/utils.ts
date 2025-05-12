e2e7a3ff55249f4a783ed70cf5f9039a0bfebd4b
Nurmukhamet Kenges <nurma192k@gmail.com>
2025-05-12 10:05:52 +0500

    add images IP

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,5 +8,5 @@ export function cn(...inputs: ClassValue[]) {
 
 export function change_minio(value: string | null | undefined) {
   if (!value) return "";
-  return value.replace(/^http:\/\/minio/, `http://${IP}`);
+  return value.replace(/^http:\/\/minio/, `http://34.40.19.17:9000`);
 }
